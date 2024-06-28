efd7258815c319c13ab9a374f745765149e96924
siva-rama-pandiyan <sivaamc15@gmail.com>
2024-06-28 23:58:20 +0530

    Post api implemented

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -13,3 +13,20 @@ export const GET = async () => {
     });
   }
 };
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 200 }
+    );
+  } catch (err: any) {
+    return new NextResponse("Error in creating users" + err.message, {
+      status: 500,
+    });
+  }
+};
